3fa5d037d54cf4ed905d01bee4bd47ceaed1c8a4
salaerts <stefan.alaerts@gmail.com>
2014-09-25 17:20:18 +0200

    top-down mergesort implementation using lists/arrays to divide and conquer

--- csharp/Algorithms.Tests/MergeSortFixture.cs
+++ csharp/Algorithms.Tests/MergeSortFixture.cs
@@ -5,12 +5,30 @@ namespace Algorithms.Tests
     public class MergeSortFixture
     {
         [Fact]
-        public void SortsUnsortedArray()
+        public void SortsUnsortedArrayWithEvenNumberOfItems()
         {
             var input = new[] {7, 4, 1, 8, 5, 2, 9, 6, 3, 0};
+            var output = MergeSort.Sort(input);
+
+            Assert.Equal(new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, output);
+        }
+
+        [Fact]
+        public void CanHandleEmptyArray()
+        {
+            var input = new int[0];
+            var output = MergeSort.Sort(input);
+
+            Assert.Equal(new int[0], output);
+        }
+
+        [Fact]
+        public void CanHandleSingleItemArray()
+        {
+            var input = new[] { 2 };
             MergeSort.Sort(input);
 
-            Assert.Equal(new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, input);
+            Assert.Equal(new[] { 2 }, input);
         }
     }
 }
--- csharp/Algorithms/MergeSort.cs
+++ csharp/Algorithms/MergeSort.cs
@@ -1,8 +1,13 @@
 ﻿namespace Algorithms
 {
     /// <summary>
-    /// 1. Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
-    /// 2. Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
+    /// Top-down implementation using lists
+    /// (creates new arrays, does not operate in-place on the input array)
+    /// 
+    /// 1. Divide the unsorted list into n sublists, 
+    ///    each containing 1 element (a list of 1 element is considered sorted).
+    /// 2. Repeatedly merge sublists to produce new sorted sublists until 
+    ///    there is only 1 sublist remaining. This will be the sorted list.
     /// 
     /// Worst-case and average running time: O(n log n)
     /// </summary>
@@ -10,7 +15,72 @@
     {
         public static int[] Sort(int[] input)
         {
-            throw new System.NotImplementedException();
+            // Base case: a list of one or less elements is already sorted
+            if (input.Length <= 1)
+            {
+                return input;
+            }
+
+            // recursive case: divide the list into equal-sized sublists
+            var middleIndex = input.Length / 2;
+            var left = new int[middleIndex];
+            var right = new int[input.Length - middleIndex];
+
+            for (var i = 0; i < middleIndex; i++)
+            {
+                left[i] = input[i];
+            }
+
+            for (var i = middleIndex; i < input.Length; i++)
+            {
+                right[i - middleIndex] = input[i];
+            }
+
+            // recursively sort both sublists
+            left = Sort(left);
+            right = Sort(right);
+
+            // conquer: merge the sorted sublists
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            var result = new int[left.Length + right.Length];
+            var numLeft = 0;
+            var numRight = 0;
+            var numResult = 0;
+
+            while (numLeft < left.Length || numRight < right.Length)
+            {
+                if (numLeft < left.Length && numRight < right.Length)
+                {
+                    if (left[numLeft] <= right[numRight])
+                    {
+                        result[numResult] = left[numLeft];
+                        numLeft++;
+                    }
+                    else
+                    {
+                        result[numResult] = right[numRight];
+                        numRight++;
+                    }
+                }
+                else if (numLeft < left.Length)
+                {
+                    result[numResult] = left[numLeft];
+                    numLeft++;
+                }
+                else if (numRight < right.Length)
+                {
+                    result[numResult] = right[numRight];
+                    numRight++;
+                }
+
+                numResult++;
+            }
+
+            return result;
         }
     }
 }
